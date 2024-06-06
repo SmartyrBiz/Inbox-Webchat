25bb7f9b80a06c5086380a5a559088bfc9c437b4
StanLachie <lachiejeffress@gmail.com>
2024-06-06 16:31:38 +1000

    Attempted build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   build: {
     lib: {
       entry: "src/main.tsx", // Your entry point
